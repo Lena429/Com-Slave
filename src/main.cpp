ac944c72a11e3fd4b28f897ce9f7d160f819fd68
Lena429 <l-frate@onu.edu>
2024-09-30 20:12:25 -0400

    updated code

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,43 @@
 #include <Wire.h>
 
 #define ADDRESS 0x12        // slave address
+const int BUFFER_SIZE = 16;
+char buffer[BUFFER_SIZE];
 
 void action(int numBytes);
+void reply();
 
 void setup(){
     Wire.begin(ADDRESS);
 
     Wire.onReceive(action);
+    Wire.onRequest(reply);
 
     Serial.begin(115200);
 }
 
 void loop(){
-  
 }
 
 void action(int numBytes){
-    int bytes;
+    int index = 0;
 
-    while(Wire.available()){
-        bytes = Wire.read();
-        Serial.println(bytes);
+    memset(buffer, 0, BUFFER_SIZE);
+
+    while(Wire.available() && index < BUFFER_SIZE - 1){
+      buffer[index] = Wire.read();
+      index++;
     }
+    buffer[index] = '\0';
 
+    String message = buffer;
+    Serial.println("Received: " + message);
+
+} 
+
+void reply(){
+    Wire.write((uint8_t*)buffer, strlen(buffer)); 
+    Serial.println("Sent: " + String((char*)buffer));
+    // Clear the buffer after sending
+    memset(buffer, 0, BUFFER_SIZE);
 }
